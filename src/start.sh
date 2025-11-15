2631c463937c97c0b6351a2140fcd3ee2746ab07
Usama Ahmed <maxpaynestory@gmail.com>
2025-11-15 23:27:59 +0500

    fixing start.sh

--- src/start.sh
+++ src/start.sh
@@ -4,17 +4,5 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
-echo "worker-comfyui: Starting ComfyUI"
-
-# Default log level
-: "${COMFY_LOG_LEVEL:=DEBUG}"
-
-# Run ComfyUI API in foreground if serving locally, otherwise run main in foreground
-if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    echo "worker-comfyui: Serving ComfyUI API locally"
-    exec python -u /comfyui/main.py --disable-auto-launch --disable-metadata --listen \
-        --verbose "${COMFY_LOG_LEVEL}" --log-stdout
-else
-    echo "worker-comfyui: Starting handler for RunPod"
-    exec python -u /handler.py
-fi
+echo "runpod-worker-comfy: Starting ComfyUI"
+python main.py --disable-auto-launch --disable-metadata --listen & python -u handler.py
