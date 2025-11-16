7b49bc87709fdfdeee6dfa22d74f58bbd619598f
Usama Ahmed <maxpaynestory@gmail.com>
2025-11-16 12:05:31 +0500

    kijai wanvideowrapper custom node added

--- scripts/comfy-node-install.sh
+++ scripts/comfy-node-install.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-# comfy-manager-set-mode: Set ComfyUI-Manager network_mode in its config.ini.
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+CUSTOM_NODES_DIR=" /comfyui/custom_nodes"
+
+mkdir -p $CUSTOM_NODES_DIR
+
+if [ ! -d "$CUSTOM_NODES_DIR/ComfyUI-WanVideoWrapper" ]; then
+  echo "Cloning ComfyUI-WanVideoWrapper..."
+  git clone https://github.com/kijai/ComfyUI-WanVideoWrapper.git "$CUSTOM_NODES_DIR/ComfyUI-WanVideoWrapper"
+  pip install -r "$CUSTOM_NODES_DIR/ComfyUI-WanVideoWrapper/requirements.txt"
+fi
